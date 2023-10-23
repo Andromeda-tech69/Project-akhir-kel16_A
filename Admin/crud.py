48d76a3545d4b35c3800b4f683067a41fe9888a9
FatiemFafa <fatiematulbalagh@gmail.com>
2023-10-23 22:29:48 +0700

    update

--- Admin/crud.py
+++ Admin/crud.py
@@ -2,67 +2,70 @@ import json,os
 
 ROOT_DIR = os.path.abspath(os.curdir)
 
-# Fungsi untuk membaca data dari file
+# Fungsi untuk membaca data dari file JSON
 def load_data():
     try:
         with open(f"{ROOT_DIR}/dataset/data.json", "r") as file:
             data = json.load(file)
     except FileNotFoundError:
-        data = []
+        data = {"kamar_pc": []}
     return data
 
-# Fungsi untuk menyimpan data ke file
+# Fungsi untuk menyimpan data ke file JSON
 def save_data(data):
     try:
         with open(f"{ROOT_DIR}/dataset/data.json", "w") as file:
             json.dump(data, file, indent=2)
-    except FileNotFoundError:
-        print("File not found. Please check the file path.")
-        raise SystemExit
-
+    except FileNotFoundError: print("File Not Found")
+        
 # Fungsi untuk menambahkan data baru
 def create_data(data):
     new_entry = {
-        "nama": input("Masukkan nama: "),
-        "umur": input("Masukkan umur: "),
+        "nomor_kamar": len(data["kamar_pc"]) + 1,
+        "jenis": input("Masukkan jenis kamar: "),
+        "akses": input("Masukkan akses kamar: "),
+        "spek_pc": input("Masukkan spesifikasi PC: "),
     }
 
-    data.append(new_entry)
+    data["kamar_pc"].append(new_entry)
     save_data(data)
     print("Data berhasil ditambahkan.")
 
 # Fungsi untuk membaca data
 def read_data(data):
-    for i, entry in enumerate(data):
-        print(f"Data ke-{i + 1}:")
-        print(f"Nama: {entry['nama']}")
-        print(f"Umur: {entry['umur']}")
+    kamar_pc = data["kamar_pc"]
+    for entry in kamar_pc:
+        print(f"Nomor Kamar: {entry['nomor_kamar']}")
+        print(f"Jenis: {entry['jenis']}")
+        print(f"Akses: {entry['akses']}")
+        print(f"Spesifikasi PC: {entry['spek_pc']}")
         print()
 
 # Fungsi untuk mengubah data
 def update_data(data):
     read_data(data)
-    index = int(input("Masukkan nomor data yang ingin diubah: ") - 1)
-    if 0 <= index < len(data):
-        data[index]["nama"] = input("Masukkan nama baru: ")
-        data[index]["umur"] = input("Masukkan umur baru: ")
-        save_data(data)
-        print("Data berhasil diubah.")
-    else:
-        print("Nomor data tidak valid.")
+    nomor_kamar = int(input("Masukkan nomor kamar yang ingin diubah: "))
+    for entry in data["kamar_pc"]:
+        if entry["nomor_kamar"] == nomor_kamar:
+            entry["jenis"] = input("Masukkan jenis kamar baru: ")
+            entry["akses"] = input("Masukkan akses kamar baru: ")
+            entry["spek_pc"] = input("Masukkan spesifikasi PC baru: ")
+            save_data(data)
+            print("Data berhasil diubah.")
+            return
+    print("Nomor kamar tidak ditemukan.")
 
 # Fungsi untuk menghapus data
 def delete_data(data):
     read_data(data)
-    index = int(input("Masukkan nomor data yang ingin dihapus: ") - 1)
-    if 0 <= index < len(data):
-        deleted_entry = data.pop(index)
-        save_data(data)
-        print("Data berikut berhasil dihapus:")
-        print(f"Nama: {deleted_entry['nama']}")
-        print(f"Umur: {deleted_entry['umur']}")
-    else:
-        print("Nomor data tidak valid.")
+    nomor_kamar = int(input("Masukkan nomor kamar yang ingin dihapus: "))
+    for entry in data["kamar_pc"]:
+        if entry["nomor_kamar"] == nomor_kamar:
+            data["kamar_pc"].remove(entry)
+            save_data(data)
+            print(f"Data nomor kamar {nomor_kamar} berhasil dihapus.")
+            return
+    print("Nomor kamar tidak ditemukan.")
 
 # Program utama
 data = load_data()
