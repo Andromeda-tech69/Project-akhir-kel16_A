7aefaaf41024f60e720d9fbc1ddfe40db42a3a70
FatiemFafa <fatiematulbalagh@gmail.com>
2023-10-25 00:44:20 +0800

    update -fafa

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-Sebelum menjalankan PROGRAM DIHARAP!! install library terlebih dahulu!!
-
-berikut tutorialnya:
-
-1. buka terminal dengan path folder ini
-2. lalu jalan kan perintah ini di terminal anda `pip install -r requirements.txt`
-3. tunggu sampai selesai
-4. jika sudah selesai, jalankan program ini
+Ini adalah project praktikum akhir
--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import subprocess,importlib,os,time
+
+# Daftar library yang diperlukan
+required_libraries = ['pwinput', 'prettytable']
+
+# Periksa dan instal library yang belum terpasang
+print('Checking library')
+time.sleep(2)
+for lib in required_libraries:
+    try:
+        importlib.import_module(lib)
+        print(f"Library {lib} sudah terinstal.")
+    except ImportError:
+        print(f"Library {lib} belum terinstal. Menginstal...")
+        subprocess.check_call(['pip', 'install', lib])
+        for i in range(3, 0, -1):
+                time.sleep(1)
+                print("Installing Library")
+        print(f"Library {lib} telah diinstal.")
+
 import Login.login as login
-import os,time
 
 if __name__ == "__main__":
     while True:
-        try: login.main()
-        except Exception as e: print(e) 
-        except KeyboardInterrupt: 
+        try:
+            login.main()
+        except Exception as e:
+            print(e)
+        except KeyboardInterrupt:
             os.system('cls')
-            print("\nKeyBoard  Interrupt")
-            for i in range(3,0,-1):
+            print("\nKeyboard Interrupt")
+            for i in range(3, 0, -1):
                 time.sleep(1)
-                print("Auto Logout In ", i )
+                print("Auto Logout In", i)
             continue
