ac1d24eb25f48dfffd2a8274cf11f706b396e079
FatiemFafa <fatiematulbalagh@gmail.com>
2023-10-25 00:15:09 +0800

    Co-authored-by: FatiemFafa <fatiematulbalagh@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import Login.login as login
+import os,time
 
 if __name__ == "__main__":
-    try: login.main()
-    except Exception as e: print(e) 
-    except KeyboardInterrupt: print("KeyBoard  Interrupt")
+    while True:
+        try: login.main()
+        except Exception as e: print(e) 
+        except KeyboardInterrupt: 
+            os.system('cls')
+            print("\nKeyBoard  Interrupt")
+            for i in range(3,0,-1):
+                time.sleep(1)
+                print("Auto Logout In ", i )
+            continue
